548265faf776714c907f028e89f8c04878ea083d
lionroy <lionroy@gmail.com>
2018-08-31 00:39:39 +0300

    commit puzzle find typo..

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return [
+        { title: 'Dune'},
+        { title: 'Return of the king'},
+        { title: 'Foundation Empire'},
+        { title: 'M.A.S.H.'},
+        { title: 'Tao Te Ching'},
+        { title: 'You have the power to heal'}
+    ];
+}
